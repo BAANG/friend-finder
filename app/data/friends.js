2d2d6918106056f83b0e1523ca96899b6180246d
BAANG <chcelestino@gmail.com>
2019-06-29 22:26:03 -0700

    Create API routing

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friendsArray = [
+    {
+        "name": "Kuma",
+        "photo": "https://i.imgur.com/cDmocRq.jpg",
+        "scores": [
+            3,
+            4,
+            5,
+            1,
+            2,
+            5,
+            1,
+            1,
+            2,
+            1,
+            5
+        ]
+    }
+];
+
+module.exports = friendsArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,14 @@
+var path = require('path');
+
+var friends = require('../data/friends.js')
+
+module.exports = function(app) {
+    app.get("/api/friends", function(req, res) { // Prints friends table array as JSON object upon GET request
+        res.json(friends);
+    })
+
+    app.post("/api/friends", function(req, res) { // Pushes POST request into friends table
+        friends.push(req.body);
+        res.json(true);
+    })
+}
