e4b2a61588b0bf931571e37b48cc05c12a6a58a7
BAANG <chcelestino@gmail.com>
2019-06-29 22:15:57 -0700

    Create html routes

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+
+// Enable data-parsing methods for Express
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Link API and HTML routes
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
+// Initiate server
+app.listen(PORT, function() {
+    console.log("Server listening on http://localhost:" + PORT)
+})
