0675051480e81d9acafb3b6e647038e88101a6cb
BAANG <chcelestino@gmail.com>
2019-07-01 12:24:29 -0700

    Create function for calculating total difference in quiz answers

--- app/data/friends.js
+++ app/data/friends.js
@@ -12,10 +12,20 @@ var friendsArray = [
             '1',
             '1',
             '2',
-            '1',
-            '5'
+            '1'
         ]
+    },
+    {
+        "name": "Chris",
+        "photo": "https://via.placeholder.com/200",
+        "scores": ['3', '4', '5', '1', '1', '4', '5', '4', '3', '5']
+    },
+    {
+        "name": "Nicole",
+        "photo": "https://via.placeholder.com/200",
+        "scores": ['2','3','4','1','5','4','3','1','2','2']
     }
+
 ];
 
 module.exports = friendsArray;
