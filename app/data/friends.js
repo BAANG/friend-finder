c3f635719c3dbe3f3b418e422c0f827e67993284
BAANG <chcelestino@gmail.com>
2019-06-30 19:01:12 -0700

    Dry up code

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,17 +3,17 @@ var friendsArray = [
         "name": "Kuma",
         "photo": "https://i.imgur.com/cDmocRq.jpg",
         "scores": [
-            3,
-            4,
-            5,
-            1,
-            2,
-            5,
-            1,
-            1,
-            2,
-            1,
-            5
+            '3',
+            '4',
+            '5',
+            '1',
+            '2',
+            '5',
+            '1',
+            '1',
+            '2',
+            '1',
+            '5'
         ]
     }
 ];
