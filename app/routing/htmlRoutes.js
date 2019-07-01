bcee2482a8e8acc25962fd7e3dd10c102db4ffdf
BAANG <chcelestino@gmail.com>
2019-06-30 17:52:53 -0700

    Create survey questions

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,11 +2,11 @@ var path = require('path');
 
 module.exports = function (app) {
 
-    app.get("/", function (req, res) {
+    app.get("/", function (req, res) { // Root directory to link to 'home.html'
         res.sendFile(path.join(__dirname, "../public/home.html"))
     });
 
-    app.get("/survey", function (req, res) {
+    app.get("/survey", function (req, res) { // Path to 'survey.html'
         res.sendFile(path.join(__dirname, "../public/survey.html"))
     })
 
